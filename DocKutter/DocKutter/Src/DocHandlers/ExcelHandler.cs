a287b79c6c39cf5dcfae5ac520e1a5f34e446516
Subho Ghosh <subho.ghosh@outlook.com>
2019-12-03 00:34:50 +0530

    Added Excel handler

---
+++ DocKutter/DocKutter/Src/DocHandlers/ExcelHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using DocKutter.Common;
+using DocKutter.Common.Utils;
+
+namespace DocKutter.DocHandlers
+{
+    public class ExcelHandler : IDocHandler
+    {
+        public string ConvertToPDF(string fileName, string outDir, bool createDir = false)
+        {
+            Preconditions.CheckArgument(fileName);
+            try
+            {
+                FileInfo inFile = new FileInfo(fileName);
+                if (!inFile.Exists)
+                {
+                    throw new FileNotFoundException("Input Excel file not found.", fileName);
+                }
+                if (createDir)
+                {
+                    if (!FileUtils.CheckDirectory(outDir))
+                    {
+                        throw new DirectoryNotFoundException(String.Format("Output directory not found/be created. [path={0}]", outDir));
+                    }
+                }
+                Application excel = new Application();
+                excel.Visible = false;
+                try
+                {
+                    Workbook workbook = excel.Workbooks.Open(inFile.FullName);
+                    try
+                    {
+                        string fname = Path.GetFileNameWithoutExtension(fileName);
+                        string outpath = String.Format("{0}/{1}.PDF", outDir, fname);
+                        LogUtils.Debug(String.Format("Generating PDF output. [path={0}]", outpath));
+
+                        workbook.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF, outpath, XlFixedFormatQuality.xlQualityStandard, true, true);
+
+                        return outpath;
+                    }
+                    finally
+                    {
+                        workbook.Close();
+                    }
+                }
+                finally
+                {
+                    excel.Quit();
+                }
+            } 
+            catch(Exception ex)
+            {
+                LogUtils.Error(ex);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ DocKutter/DocKutterTests/ExcelhandlerTest.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using Xunit;
+using DocKutter.Common.Utils;
+
+namespace DocKutter.DocHandlers
+{
+    public class ExcelHandlerTest
+    {
+        private const string SOURCE_FILE = @"..\..\..\Resources\Excel\FinancialSample.xlsx";
+        private const string SOURCE_FILE_GEAR_CHARTS = @"..\..\..\Resources\Excel\GearChart.xls";
+        private const string SOURCE_FILE_COUNTRY_CHARTS = @"..\..\..\Resources\Excel\CountryChart.xls";
+        private static string OUTPUT_DIR_NAME = System.Guid.NewGuid().ToString();
+
+        [Fact]
+        public void ConvertToPDF()
+        {
+            try
+            {
+                LogUtils.Debug(String.Format("Current Directory: {0}", Directory.GetCurrentDirectory()));
+                string outDir = FileUtils.GetTempDirectory(OUTPUT_DIR_NAME);
+
+                ExcelHandler handler = new ExcelHandler();
+                string outfile = handler.ConvertToPDF(SOURCE_FILE, outDir, true);
+            }
+            catch (Exception ex)
+            {
+                LogUtils.Error(ex);
+                throw ex;
+            }
+        }
+
+        [Fact]
+        public void ConvertToPDF_GearCharts()
+        {
+            try
+            {
+                LogUtils.Debug(String.Format("Current Directory: {0}", Directory.GetCurrentDirectory()));
+                string outDir = FileUtils.GetTempDirectory(OUTPUT_DIR_NAME);
+
+                ExcelHandler handler = new ExcelHandler();
+                string outfile = handler.ConvertToPDF(SOURCE_FILE_GEAR_CHARTS, outDir, true);
+            }
+            catch (Exception ex)
+            {
+                LogUtils.Error(ex);
+                throw ex;
+            }
+        }
+
+        [Fact]
+        public void ConvertToPDF_CountryCharts()
+        {
+            try
+            {
+                LogUtils.Debug(String.Format("Current Directory: {0}", Directory.GetCurrentDirectory()));
+                string outDir = FileUtils.GetTempDirectory(OUTPUT_DIR_NAME);
+
+                ExcelHandler handler = new ExcelHandler();
+                string outfile = handler.ConvertToPDF(SOURCE_FILE_COUNTRY_CHARTS, outDir, true);
+            }
+            catch (Exception ex)
+            {
+                LogUtils.Error(ex);
+                throw ex;
+            }
+        }
+    }
+}
