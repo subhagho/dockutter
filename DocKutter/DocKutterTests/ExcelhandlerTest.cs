faa664c9ea2b4c1043b97c0ad78911a42e7fb5aa
Subho Ghosh <subho.ghosh@outlook.com>
2019-12-10 20:12:26 +0530

    Sync commit.

--- DocKutter/DocKutterTests/ExcelhandlerTest.cs
+++ DocKutter/DocKutterTests/ExcelhandlerTest.cs
@@ -1,14 +1,11 @@
 using System;
 using System.IO;
-using System.Threading;
-using System.Collections.Generic;
 using Xunit;
 using DocKutter.Common.Utils;
-using DocKutter.Executor;
 
 namespace DocKutter.DocHandlers
 {
-    public class ExcelHandlerTest
+    public class ExcelhandlerTest
     {
         private const string SOURCE_FILE = @"..\..\..\Resources\Excel\FinancialSample.xlsx";
         private const string SOURCE_FILE_GEAR_CHARTS = @"..\..\..\Resources\Excel\GearChart.xls";
@@ -20,22 +17,11 @@ namespace DocKutter.DocHandlers
         {
             try
             {
-                ThreadPool.SetMaxThreads(8, 16);
-
                 LogUtils.Debug(String.Format("Current Directory: {0}", Directory.GetCurrentDirectory()));
                 string outDir = FileUtils.GetTempDirectory(OUTPUT_DIR_NAME);
 
-                DocRequestHandler handler = new DocRequestHandler();
-                handler.Init();
-
-                List<ManualResetEvent> events = new List<ManualResetEvent>();
-
-                DocResponseHandler responseHandler = ResponseCallback;
-                events.Add(handler.Run(DocRequestHandler.DOC_HANDLER_EXCEL, SOURCE_FILE, outDir, responseHandler));
-                events.Add(handler.Run(DocRequestHandler.DOC_HANDLER_EXCEL, SOURCE_FILE_GEAR_CHARTS, outDir, responseHandler));
-                events.Add(handler.Run(DocRequestHandler.DOC_HANDLER_EXCEL, SOURCE_FILE_COUNTRY_CHARTS, outDir, responseHandler));
-
-                WaitHandle.WaitAll(events.ToArray());
+                ExcelHandler handler = new ExcelHandler();
+                string outfile = handler.ConvertToPDF(SOURCE_FILE, outDir, true);
             }
             catch (Exception ex)
             {
@@ -44,16 +30,39 @@ namespace DocKutter.DocHandlers
             }
         }
 
-
-        private void ResponseCallback(ProcessResponse response)
+        [Fact]
+        public void ConvertToPDF_GearCharts()
         {
-            if (response.Error != null)
+            try
             {
-                LogUtils.Error(response.Error);
+                LogUtils.Debug(String.Format("Current Directory: {0}", Directory.GetCurrentDirectory()));
+                string outDir = FileUtils.GetTempDirectory(OUTPUT_DIR_NAME);
+
+                ExcelHandler handler = new ExcelHandler();
+                string outfile = handler.ConvertToPDF(SOURCE_FILE_GEAR_CHARTS, outDir, true);
             }
-            else
+            catch (Exception ex)
             {
-                LogUtils.Info(String.Format("Processed Document. [source={0}][output={1}]", response.SourceDoc, response.OutputFile));
+                LogUtils.Error(ex);
+                throw ex;
+            }
+        }
+
+        [Fact]
+        public void ConvertToPDF_CountryCharts()
+        {
+            try
+            {
+                LogUtils.Debug(String.Format("Current Directory: {0}", Directory.GetCurrentDirectory()));
+                string outDir = FileUtils.GetTempDirectory(OUTPUT_DIR_NAME);
+
+                ExcelHandler handler = new ExcelHandler();
+                string outfile = handler.ConvertToPDF(SOURCE_FILE_COUNTRY_CHARTS, outDir, true);
+            }
+            catch (Exception ex)
+            {
+                LogUtils.Error(ex);
+                throw ex;
             }
         }
     }
